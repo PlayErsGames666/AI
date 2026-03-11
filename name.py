326e47d069012b9388e7a1ba44096d49956d1e40
PlayErsGames666 <playersgames.ru@gmail.com>
2026-03-11 21:45:20 +0500

    Refresh gitignore.

---
+++ name.py
@@ -0,0 +1,5 @@
+import random
+
+a = random.choice(range(1, 141))
+
+print(a)
---
+++ position.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+
+time.sleep(3)  # даёт 3 секунды на то, чтобы навести курсор на точку
+print(pyautogui.position())
